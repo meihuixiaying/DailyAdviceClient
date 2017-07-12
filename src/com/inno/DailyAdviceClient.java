dbe4a9ced7aa252ef820450f11ccaca0f1f900d2
wiggens <wangjinlong@innotechx.com>
2017-07-12 18:19:04 +0800

    first commit

---
+++ src/com/inno/DailyAdviceClient.java
@@ -0,0 +1,25 @@
+package com.inno;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class DailyAdviceClient {
+	public void go() {
+		try {
+			Socket s = new Socket("198.12.101.135", 22);
+			InputStreamReader streamReader = new InputStreamReader(s.getInputStream());
+			BufferedReader reader = new BufferedReader(streamReader);
+			String advice = reader.readLine();
+			System.out.println("Today you should: " + advice);
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		new DailyAdviceClient().go();
+	}
+}
